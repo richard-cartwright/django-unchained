5d41d35edea7651e67a69aaf295c88d2aa070a35
richard <richard@fraclog.co>
2018-12-12 13:48:25 +0000

    End of Part 6

--- polls/views.py
+++ polls/views.py
@@ -14,12 +14,10 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """
-	Return the last five published questions (not including those set to be
-    	published in the future).
-    	"""
-    	return Question.objects.filter(
-        	pub_date__lte=timezone.now()
-    	).order_by('-pub_date')[:5]
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
